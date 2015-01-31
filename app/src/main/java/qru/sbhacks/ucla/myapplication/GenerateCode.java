00b183f12d1666d805b1c277136de9440385524c
KyCodeHuynh <KyCodeHuynh@gmail.com>
2015-01-30 23:21:45 -0800

    Added GenerateCode activity with checklist.

--- app/src/main/java/qru/sbhacks/ucla/myapplication/GenerateCode.java
+++ app/src/main/java/qru/sbhacks/ucla/myapplication/GenerateCode.java
@@ -4,24 +4,21 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 
-public class HomeScreen extends ActionBarActivity {
-
-    // get text via textView library
+public class GenerateCode extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_screen);
+        setContentView(R.layout.activity_generate_code);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home_screen, menu);
+        getMenuInflater().inflate(R.menu.menu_generate_code, menu);
         return true;
     }
 
